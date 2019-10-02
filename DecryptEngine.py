59519b804c9eceddbc6a95118dbf96b95bcd110a
MarvinKweyu <mkweyu1@gmail.com>
2019-10-02 08:02:52 +0300

    Follow pydoc naming convantions.

--- DecryptEngine.py
+++ DecryptEngine.py
@@ -1,44 +1,47 @@
 #!/usr/bin/python
 
 
-BLOCKSIZE = 16 
+BLOCKSIZE = 16
+
 
 class DecryptEngine():
 
-    def __init__(self, padChecker):
-        self.padChecker = padChecker
+    def __init__(self, pad_checker):
+        self.pad_checker = pad_checker
 
-    def decryptAtIndex(self, ciphertext, index):
-        testCt = ciphertext
+    def decrypt_at_index(self, ciphertext, index):
+        test_ct = ciphertext
         for guess in range(256):
-            testCt[index] = chr(guess)		
-            if self.padChecker.hasValidPadding(testCt):
+            test_ct[index] = chr(guess)
+            if self.pad_checker.hasValidPadding(test_ct):
                 return guess
-        raise RuntimeError("[!] Found no valid padding, is PadChecker implemented correctly?")
+        raise RuntimeError(
+            "[!] Found no valid padding, is PadChecker implemented correctly?")
 
-    def decryptBlock(self, block):
+    def decrypt_block(self, block):
         iv = bytearray("\x00"*BLOCKSIZE)
         intermediate = bytearray("\x00"*BLOCKSIZE)
         for i in range(BLOCKSIZE):
             for j in range(i):
                 iv[(BLOCKSIZE-1)-j] = intermediate[(BLOCKSIZE-1)-j] ^ (i+1)
 
-            ep = self.decryptAtIndex(iv+block, (BLOCKSIZE-1)-i)
+            ep = self.decrypt_at_index(iv+block, (BLOCKSIZE-1)-i)
             intermediate[(BLOCKSIZE-1)-i] = ep ^ (i+1)
-            print("intermediate: {}".format([hex(x)[2:] for x in intermediate]))
+            print("intermediate: {}".format(
+                [hex(x)[2:] for x in intermediate]))
         return intermediate
 
-    def getIntermediate(self, ciphertext):
+    def get_intermediate(self, ciphertext):
         key = ""
         blocks = len(ciphertext)/BLOCKSIZE
         for i in range(blocks):
-            blockStart = len(ciphertext)-(i+1)*BLOCKSIZE
-            blockEnd = len(ciphertext)-(i*BLOCKSIZE)
-            key = self.decryptBlock(ciphertext[blockStart:blockEnd]) + key
+            block_start = len(ciphertext)-(i+1)*BLOCKSIZE
+            block_end = len(ciphertext)-(i*BLOCKSIZE)
+            key = self.decrypt_block(ciphertext[block_start:block_end]) + key
         return key
 
     def decrypt(self, ciphertext):
-        key = self.getIntermediate(ciphertext)
+        key = self.get_intermediate(ciphertext)
         plaintext = ""
         for i in range(len(ciphertext)-BLOCKSIZE):
             plaintext += chr(ord(ciphertext[i]) ^ key[i+BLOCKSIZE])
