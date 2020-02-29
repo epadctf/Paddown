3d98c33a23bc18bd0551bf4560a421d3bdda47d3
Kristian Theilgaard <kristian.theilgaard@gmail.com>
2020-02-29 11:47:58 +0100

    Add python help tools

--- PadDown/tests/test_decrypt_engine.py
+++ PadDown/tests/test_decrypt_engine.py
@@ -1,6 +1,5 @@
-from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.Padding import unpad
 from PadDown.decrypt_engine import DecryptEngine, PadChecker
-
 from PadDown.examples.vulnerable_encryption_service import VulnerableEncryptionService
 
 VEC = VulnerableEncryptionService()
@@ -22,15 +21,14 @@ class TestDecryptEngine:
 
         decrypt_engine = DecryptEngine(TestPadChecker())
         decrypt_engine.decrypt_at_index(bytearray(b"\x00"), 0)
-    
+
     def test_decrypt_block(self):
         class TestPadChecker(PadChecker):
             def has_valid_padding(self, ciphertext):
                 return ciphertext == b"\x05"
 
-
     def test_complete_run(self):
-        plaintext_original = bytearray("This is a padded plaintext", encoding='ascii')
+        plaintext_original = bytearray("This is a padded plaintext", encoding="ascii")
 
         # Assert that the plaintext is padded
         assert len(plaintext_original) % 16 != 0
